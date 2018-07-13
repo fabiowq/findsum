566d9dfb9cf6601af5e2143a5c520fe33f77547f
lsad <lucasalmeida.gft@sulamerica.com.br>
2018-07-13 11:33:24 -0300

    LSAD - Alteração de método recursivo

--- src/main/java/com/challenge/findsum/FindSumLsadImpl.java
+++ src/main/java/com/challenge/findsum/FindSumLsadImpl.java
@@ -1,30 +1,35 @@
 package com.challenge.findsum;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Random;
 
+@Primary
 @Component
 public class FindSumLsadImpl implements FindSum {
 
   public boolean find(int[] A, int x) {
+
     if (A.length <= 1) return false;
-    Arrays.sort(A);
-    for (int i = 0; i < A.length; i++ ) {
+    for (int i = 0; i < A.length; i++) {
       int soma = A[i];
       for (int j = i + 1; j < A.length; j++)
-        if ( verificaArr(soma, j, A, x) ) return true;
+        if (verificaArr(soma, j, A, x)) return true;
     }
 
     return false;
   }
 
   boolean verificaArr(int somatorio, int pos, int[] A, int x) {
+
     for (int j = pos; j < A.length; j++) {
       somatorio += A[j];
       if (somatorio == x) return true;
       if (somatorio > x) return false;
-      if (verificaArr(somatorio, j+1, A, x)) return true;
+      for (int k = j + 1; k < A.length; k++)
+        if (verificaArr(somatorio, k, A, x)) return true;
     }
 
     return false;
